e63caa8ab982b182acd9632caf39ab6028795201
NeilChavez <lolochavez22@icloud.com>
2024-12-11 00:06:38 +0100

    feat: define application routes for CRUD operations

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require "../vendor/autoload.php";
+require "../includes/app.php";
+
+use MVC\Router;
+use Controller\PageController;
+use Controller\PostController;
+
+$router = new Router();
+
+
+$router->get("/home", [PageController::class, "index"]);
+$router->get("/post", [PageController::class, "singlePost"]);
+$router->get("/post/create", [PostController::class, "create"]);
+$router->post("/post/create", [PostController::class, "create"]);
+$router->get("/posts/admin", [PostController::class, "admin"]);
+$router->get("/post/update", [PostController::class, "update"]);
+
+$router->checkRoutes();
