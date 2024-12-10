5993ee2dd234178117ab3f396725feb9e39a8d66
NeilChavez <lolochavez22@icloud.com>
2024-12-10 18:28:19 +0100

    feat: add handling for deletion and reuse of feature image in hidden input

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -11,15 +11,17 @@ class PostController
 {
   public static function create(Router $router)
   {
+    $post = new Post();
     $errors = [];
     if ($_SERVER["REQUEST_METHOD"] === "POST") {
-      
+
       $_POST["content"] = trim($_POST["content"]);
-  
+
       $post = new Post($_POST);
 
       $tempFile = $_FILES["feature_image"]["tmp_name"];
 
+      //user put a new image to upload
       if (!empty($tempFile)) {
 
         // create a manager with a driver
@@ -28,7 +30,7 @@ class PostController
         $image = $manager->read($tempFile);
 
         $imagesDirectory = $_SERVER["DOCUMENT_ROOT"] . "/images/";
-        
+
         // check if there is a directory images. If is not, create a directory
         if (!is_dir($imagesDirectory)) {
 
@@ -43,15 +45,29 @@ class PostController
 
         $post->feature_image = $imageName;
 
+        // there is a image in the hidden input, so user we delete it
+        if (!empty($_POST["feature_image"])) {
+
+          $previusImage = $_POST["feature_image"];
+          unlink($imagesDirectory . $previusImage);
+        }
+      }
+      // user hasn't uploaded a new image, we have one in the input hidden
+      if (empty($tempFile) && !empty($_POST["feature_image"])) {
+
+        $post->feature_image = $_POST["feature_image"];
       }
 
-     $errors =  $post->getErrors();
+      $errors =  $post->getErrors();
 
       if (empty($errors)) {
 
-        $post->create();
-      }
+        $result = $post->create();
 
+        if ($result) {
+          header("Location: /post/create?message=1");
+        }
+      }
     }
 
     $router->render("/posts/create", [
