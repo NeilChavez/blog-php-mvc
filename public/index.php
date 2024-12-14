81434b7dd9f6d7c1e5b41c16639790b81b5bc392
NeilChavez <lolochavez22@icloud.com>
2024-12-14 15:37:03 +0100

    feat: add update route for posts management

--- public/index.php
+++ public/index.php
@@ -16,5 +16,6 @@ $router->get("/post/create", [PostController::class, "create"]);
 $router->post("/post/create", [PostController::class, "create"]);
 $router->get("/posts/admin", [PostController::class, "admin"]);
 $router->get("/post/update", [PostController::class, "update"]);
+$router->post("/post/update", [PostController::class, "update"]);
 
 $router->checkRoutes();
